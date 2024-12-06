d3a9f1dd6561edabbdcb4d449eacc6dfa1502e6c
LucasBbe <lucas.boube@gmail.com>
2024-12-06 12:26:09 +0100

    merge resolving

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,7 +3,7 @@ import { Outlet } from "react-router-dom";
 
 import Footer from "./components/Footer/Footer";
 
-import Header from "./components/Header/Header";
+import Header from "./components/header/Header";
 
 function App() {
   return (
