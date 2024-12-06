8f994afb3b8a6ed5da7ea130a417e834da443f00
LucasBbe <lucas.boube@gmail.com>
2024-12-06 10:30:06 +0100

    adding news and about us page, also adding search bar

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,18 +1,8 @@
 import "./styles/Globals.css";
-import Caroussel from "./components/Caroussel/Caroussel.tsx";
-import { Card } from "./components/card";
-import Footer from "./components/footer/Footer";
+import { Outlet } from "react-router-dom";
+import Footer from "./components/Footer/Footer";
 
 import Header from "./components/header/Header";
-const article = {
-  title: "Breaking News: New Developments in Technology",
-  urlToImage:
-    "https://c0.lestechnophiles.com/images.frandroid.com/wp-content/uploads/2024/11/toaster-xbox-series-s-cyber-monday-2024.jpg?resize=1600,900&key=3c2ed27a&watermark",
-  author: "BBC News",
-  description:
-    "Recent advancements in AI are changing the way we interact with machines.",
-  publishedAt: "2024-12-03T13:27:40Z",
-};
 
 function App() {
   return (
@@ -20,8 +10,11 @@ function App() {
       <header>
         <Header />
       </header>
-      <Caroussel />
-      <Card article={article} />
+
+      <main className="the-main">
+        <Outlet />
+      </main>
+
       <Footer />
     </>
   );
