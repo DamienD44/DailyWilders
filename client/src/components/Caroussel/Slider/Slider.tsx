cd6e7346a7ac4f372abc29b462b86eccd6d15a2b
Fabrice Atlan <fabrice.atlan.56@gmail.com>
2024-12-06 12:18:57 +0100

    Formating is done.

--- client/src/components/Caroussel/Slider/Slider.tsx
+++ client/src/components/Caroussel/Slider/Slider.tsx
@@ -2,7 +2,6 @@ interface SliderProps {
   urlToImage: string;
 }
 export default function Slider({ urlToImage }: SliderProps) {
-  console.info(urlToImage);
   return (
     <figure className="caroussel-figure">
       <img src={urlToImage} alt="" className="caroussel-img" />
