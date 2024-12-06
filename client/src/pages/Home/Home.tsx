965a7012fe8a505cbdbb1071156ec61f1e7b553e
GaeBolga <benjamin.lefort@mail.com>
2024-12-06 11:27:13 +0100

    ajout du map

--- client/src/pages/Home/Home.tsx
+++ client/src/pages/Home/Home.tsx
@@ -1,22 +1,29 @@
+import { useLoaderData } from "react-router-dom";
 import Caroussel from "../../components/Caroussel/Caroussel.tsx";
 import { Card } from "../../components/card/Card.tsx";
 
-const article = {
-  title: "Breaking News: New Developments in Technology",
-  urlToImage:
-    "https://c0.lestechnophiles.com/images.frandroid.com/wp-content/uploads/2024/11/toaster-xbox-series-s-cyber-monday-2024.jpg?resize=1600,900&key=3c2ed27a&watermark",
-  author: "BBC News",
-  description:
-    "Recent advancements in AI are changing the way we interact with machines.",
-  publishedAt: "2024-12-03T13:27:40Z",
+type Article = {
+  title: string;
+  urlToImage: string;
+  author: string;
+  description: string;
+  publishedAt: string;
+};
+
+type LoaderData = {
+  articles: Article[];
 };
 
 export default function Home() {
+  const { articles } = useLoaderData() as LoaderData;
+
   return (
     <>
       <main className="main-container">
         <Caroussel />
-        <Card article={article} />
+        {articles.map((article) => (
+          <Card key={article.title} article={article} />
+        ))}
       </main>
     </>
   );
